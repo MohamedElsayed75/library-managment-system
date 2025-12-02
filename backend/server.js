f377bc28b874ae8f0ba9b2bd3fe32d89ebf9dacb
Lighteldin <noureldina2005@gmail.com>
2025-12-02 14:01:01 +0200

    rename backend.js to server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors()); // allow all origins
+app.use(express.json()); // parse JSON bodies
+
+const authRouter = require("./routes/auth");
+app.use("/auth", authRouter);
+
+// const booksRouter = require("./routes/books");
+// app.use("/books", booksRouter);
+
+// const loansRouter = require("./routes/loans");
+// app.use("/loans", loansRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
